416521f44859569bb8b392798c2392f767d5ed34
Renan Martins <renanmartins1999@hotmail.com>
2021-04-08 21:00:46 -0300

    Created bubbleSort function

--- C/main.c
+++ C/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(){
+void bubbleSort(int *v, int tam)
+{
+	//printf("%d", v[sizeof(v)/sizeof(int) - 1]);
+	for(int i = 0; i <= tam; i++)
+	{
+		//printf("%d\n", v[i]);
+	}
+}
 
+int main()
+{
+	int v[] = { 1, 10, 2, 3, 4, 5 };
+	int tamanho = sizeof(v)/sizeof(int) - 1;
+	//printf("%d", bubbleSort(v));
+	bubbleSort(v, tamanho);
+
+	//Caso esteja rodando o codigo no Windows descomentar a linha abaixo
+	//system("pause");
 	return 0;
 }
