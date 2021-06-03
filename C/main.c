d868469985acf1eb8632d629d2d554686d0fbab1
Renan Martins <renanmartins1999@hotmail.com>
2021-06-03 10:28:41 -0300

    Implementing bubble Sort

--- C/main.c
+++ C/main.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 
+// Bubble Sort
+// Complexity O(n²)
 void bubbleSort(int *v, int tam)
 {
-	//printf("%d", v[sizeof(v)/sizeof(int) - 1]);
-	for(int i = 0; i <= tam; i++)
+	int temp;
+	
+	for(int i = 0; i < tam; i++)
 	{
 		//printf("%d\n", v[i]);
+		if(v[i] > v[i+1]){
+			temp = v[i];
+			v[i] = v[i+1];
+			v[i+1] = temp;
+		}
 	}
 }
 
 int main()
 {
+	// Declarando um array
 	int v[] = { 1, 10, 2, 3, 4, 5 };
+	// Tamanho do array
 	int tamanho = sizeof(v)/sizeof(int) - 1;
-	//printf("%d", bubbleSort(v));
+
+	// Procedimento para ordenar o array
 	bubbleSort(v, tamanho);
+
+	// Imprindo os elementos do array
+	for(int i = 0; i < tamanho; i++)
+	{	
+		printf("%d ", v[i]);
+	}
 
 	//Caso esteja rodando o codigo no Windows descomentar a linha abaixo
 	//system("pause");
--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <iomanip>
+#include <chrono>
 
 int main()
 {
-    std::vector<int> v(6);
-    v = {6, 4, 3, 2, 1, 9};
+    // Declaring a vector int of 6 positions
+    std::vector<int> v(12);
+    v = {6, 4, 3, 2, 1, 9, 0, -2 , 100, 75, 90, 32};
 
+    // Counting the elapsed time of the sort algorithm
+    auto start = std::chrono::steady_clock::now();
     std::stable_sort(v.begin(), v.end());
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
 
+    // Print values inside the vector
     for(int i: v)
     {
         std::cout << i << " " ;
     }
+
+    std::cout << std::fixed;
+    std::cout << std::endl << "Elapsed Time of Stable Sort: " << elapsed_seconds.count() << std::endl;
 
 
 }
