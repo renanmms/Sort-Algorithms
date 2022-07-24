9e3c76a8329b62f7c35e6c08eaa786794e9163c7
Renan Martins <renanmartins1999@hotmail.com>
2022-07-23 22:03:17 -0300

    Implemented Bubble Sort in C#

---
+++ Bubble_Sort/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var arr = new int[]{19, 1, 9, 7, 3, 10, 13, 15, 8, 12};
+
+foreach(var i in arr){
+    Console.Write($"| {i} ");
+}
+
+BubbleSort(arr);
+
+Console.WriteLine("\n Sorting......");
+
+foreach(var i in arr){
+    Console.Write($"| {i} ");
+}
+
+static void BubbleSort(int[] arr){
+    int temp = 0;
+    for(int i = 0; i < arr.Count() - 1; i++){
+        for(int j = 0; j < arr.Count() - (i + 1); j++){
+            if(arr[j] > arr[j+1]){ 
+                temp = arr[j+1]; 
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }    
+}
