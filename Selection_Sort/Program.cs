16b213b4254a4d4f11ceec33216b56dd5d83c6c0
Renan Martins <renanmartins1999@hotmail.com>
2022-07-26 22:40:36 -0300

    Finished Selection Sort

---
+++ Selection_Sort/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var arr = new int[]{13, 19, 18, 4, 10};
+
+foreach(var i in arr){
+    Console.Write($"| {i} ");
+}
+
+SelectionSort(arr);
+Console.WriteLine("\nSorting......");
+
+foreach(var i in arr){
+    Console.Write($"| {i} ");
+}
+
+static void SelectionSort(int[] arr){
+    int key = 0;
+    int min = int.MaxValue;
+    int index = -1;
+
+    for(int i = 0; i < arr.Count(); i++){
+        key = arr[i]; 
+        for(int j = i; j < arr.Count(); j++){
+            if(min > arr[j]){
+                min = arr[j]; 
+                index = j;
+            }
+        }
+        arr[i] = min; 
+        arr[index] = key;
+        min = int.MaxValue;
+    }
+
+}
