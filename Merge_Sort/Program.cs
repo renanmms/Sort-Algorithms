f2383bca6d0ffae410986ec1a649cbbe58a5343e
Renan Martins <renanmartins1999@hotmail.com>
2023-05-01 00:31:57 -0300

    Initializing array

--- Merge_Sort/Program.cs
+++ Merge_Sort/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var array = new int[]{14, 7, 3, 12, 9, 11, 6, 2};
+
+DisplayArray(array);
+
+void MergeSort(int[] array){
+    int p = 0;
+    int r = 0;
+    int q = (p+r)/2;
+}
+
+void DisplayArray(int[] array){
+    foreach(var element in array){
+        Console.Write($"{element} ");
+    }
+
+    Console.WriteLine();
+}
